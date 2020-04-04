d42e813efb3a4ccf7b34aab10f76028789149029
jimmim <kalinin_av@list.ru>
2020-04-04 12:05:34 +0300

    lesson01

---
+++ task05/Main.java
@@ -0,0 +1,12 @@
+package task05;
+
+public class Main {
+    public static void main(String[] args){
+        int a=2020;
+        method(a);
+    }
+
+    public static void method(int a){
+        System.out.println((a%400==0) || (a%4==0 && a%100!=0)?"високосный":"не високосный");
+    }
+}
